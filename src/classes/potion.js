a18af6671a397bea935cd54fcd34b5033962efe1
Matt Rosen <matt@mrosen.com>
2019-09-10 11:28:53 -0400

    Added get active potion call

--- src/classes/potion.js
+++ src/classes/potion.js
@@ -15,4 +15,14 @@ splinterlands.Potion = class {
 	get charges_remaining() {
 		return this.player_potion.charges_remaining || 0;
 	}
+
+	get image_url() {
+		return `https://s3.amazonaws.com/steemmonsters/website/ui_elements/shop/potions/potion_${this.id}.png`;
+	}
+
+	static async get_active(type) {
+		let potions = await splinterlands.get_potions();
+		let active_potion = potions.slice().sort((a, b) => b.value - a.value).find(p => p.subtype == type && p.charges_remaining > 0);
+		return active_potion || potions.find(p => p.subtype == type);
+	}
 }
