a39823dd89a936bc4a6f8d395ab303bd86ec6821
Matt Rosen <matt@mrosen.com>
2019-12-16 09:51:16 -0500

    Added leaderboard api call

--- src/splinterlands.js
+++ src/splinterlands.js
@@ -595,10 +595,15 @@ var splinterlands = (function() {
 		return response;
 	}
 
+	async function get_leaderboard(season) {
+		return (await api('/players/leaderboard', { season })).map(p => new splinterlands.Player(p));
+	}
+
 	return { 
 		init, api, login, logout, send_tx, send_tx_wrapper, load_collection, group_collection, get_battle_summoners, get_battle_monsters, get_card_details, 
 		log_event, load_market, steem_payment, has_saved_login, create_account_email, email_login, check_promo_code, redeem_promo_code, reset_password,
 		load_market_cards, load_card_lore, group_collection_by_card, get_available_packs, get_potions, wait_for_match, wait_for_result, battle_history,
+		get_leaderboard,
 		get_config: () => _config,
 		get_settings: () => _settings,
 		get_player: () => _player,
