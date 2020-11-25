3055343e8f787d79d3c6a180ffc43de37181b119
Ulid J <ulid@yahoo.com>
2020-11-25 18:35:16 -0500

    Add web3 payment methods. Fixed some error messaging

--- src/blockchain/ethereum.js
+++ src/blockchain/ethereum.js
@@ -2,6 +2,45 @@ if(!window.splinterlands)
 	window.splinterlands = {};
 
 window.splinterlands.ethereum = (function() {
+
+	// auto-populate a transfer in Ethereum web3 wallet
+	async function sendFromWeb3(to, amount) {
+		if (!window.web3)
+			return ({ "error" : true, "message": "web3 not found" });
+
+		try {
+			await window.web3.givenProvider.enable();
+
+			if(!window.web3.eth.accounts.givenProvider.selectedAddress) {
+				return ({ "error" : true, "message": "unknown error, please try again" });
+			}
+
+			window.web3.eth.sendTransaction({
+				from: window.web3.eth.accounts.givenProvider.selectedAddress,
+				to: to,
+				gas: 21000,
+				value: window.web3.utils.toWei(amount, 'ether')
+			})
+			.on('transactionHash', (hash) => { return hash })
+			.on('error', (error, receipt) => { 
+				if(typeof error === 'string')
+					return ({ "error" : true, "message": error });
+				else 
+					return error;			
+			});
+		} catch(e) {
+			console.log(e); 
+			if(typeof e === 'string')
+				return ({ "error" : true, "message": e });
+			else 
+				return e; 
+		}
+	}	
+
+
+	/**
+	 * Public functions
+	 */
 	async function sendTransaction(signed_tx) {
 		// Broadcast the transaction
 		return await splinterlands.api('/transactions/broadcast_eth_tx', { signed_tx });
@@ -13,7 +52,7 @@ window.splinterlands.ethereum = (function() {
 
 	async function getIdentity() {
 		if (!window.web3)
-			return ({ "error" : true, "web3 not found": e });
+			return ({ "error" : true, "message": "web3 not found" });
 
 		try {
 			const publicKey = (await web3.eth.givenProvider.enable())[0];
@@ -33,7 +72,7 @@ window.splinterlands.ethereum = (function() {
 
 	async function web3Auth(type) {
 		if (!window.web3)
-			return ({ "error" : true, "web3 not found": e });
+			return ({ "error" : true, "message": "web3 not found" });
 
 		try {
 			const enableRes = await web3.eth.givenProvider.enable();
@@ -52,5 +91,17 @@ window.splinterlands.ethereum = (function() {
 		}
 	}
 
-	return { sendTransaction, hasWeb3Obj, getIdentity, web3Auth };
+	async function web3Pay(to, amount) {
+		try {
+			await sendFromWeb3(to, amount);			
+		} catch (e)  {
+			console.log(e); 
+			if(typeof e === 'string')
+				return ({ "error" : true, "message": e });
+			else 
+				return e;
+		}
+	}
+
+	return { sendTransaction, hasWeb3Obj, getIdentity, web3Auth, web3Pay };
 })();
