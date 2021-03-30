eb9a94d2b9b65dbef8c7d54c85db444499fbe307
Ulid J <ulid@yahoo.com>
2021-03-30 15:32:46 -0400

    Updated EOS api node

--- src/blockchain/eos.js
+++ src/blockchain/eos.js
@@ -8,7 +8,7 @@ window.splinterlands.eos = (function() {
 			eos_network: {
 				blockchain: 'eos',
 				protocol: 'https',
-				host: 'nodes.get-scatter.com',
+				host: 'eos.greymass.com',
 				port: 443,
 				chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
 			}
