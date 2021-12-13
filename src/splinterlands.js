2a903c6c9ba68475d9d5a9bc74a46fa340d77008
UJ <ulid@yahoo.com>
2021-12-12 22:02:59 -0500

    Updated card lore call

--- src/splinterlands.js
+++ src/splinterlands.js
@@ -798,8 +798,10 @@ var splinterlands = (function() {
 
 	let _lore = {};
 	async function load_card_lore(card_detail_id) {
-		if(!_lore[card_detail_id])
-			_lore[card_detail_id] = (await api('/cards/lore', { card_detail_id })).text;
+		if(!_lore[card_detail_id]) {
+			_lore[card_detail_id] = await api('/cards/lore', { card_detail_id });
+		}
+			
 
 		return _lore[card_detail_id];
 	}
